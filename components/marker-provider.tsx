b5b794d73d4ff8632e14e3a6136eb3bf35ec1191
Ryan Holder <ryanh@wearedauntless.com>
2025-12-02 13:44:13 +0000

    Update figma cursor example boilerplate

---
+++ components/marker-provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+import markerSDK from "@marker.io/browser";
+
+export function MarkerProvider() {
+  useEffect(() => {
+    async function initMarker() {
+      try {
+        await markerSDK.loadWidget({
+          project: "692dc1a10623ad511cb676db",
+        });
+      } catch (error) {
+        console.error("Failed to load Marker.io widget:", error);
+      }
+    }
+
+    initMarker();
+  }, []);
+
+  return null;
+}
+
