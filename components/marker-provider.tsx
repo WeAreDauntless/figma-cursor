56a346f9e2dc7e404d7f2a597bfcc21d987dd33a
Ryan Holder <ryanh@wearedauntless.com>
2025-12-03 12:32:03 +0000

    commit all changes

--- components/marker-provider.tsx
+++ components/marker-provider.tsx
@@ -5,6 +5,11 @@ import markerSDK from "@marker.io/browser";
 
 export function MarkerProvider() {
   useEffect(() => {
+    // Don't load marker.io in development
+    if (process.env.NODE_ENV === "development") {
+      return;
+    }
+
     async function initMarker() {
       try {
         await markerSDK.loadWidget({
